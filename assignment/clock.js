ed13da17ce2bbf212c11107d8074aa582d120e26
manishvan46 <115293970+manishvan46@users.noreply.github.com>
2022-12-10 10:44:54 +0530

    Add files via upload

---
+++ assignment/clock.js
@@ -0,0 +1,19 @@
+var a =null;
+a=setTimeout(function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('x1').innerHTML =  h + ":" + m + ":" + s;
+  },1000);
+  
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  
+    return i;
+  }
+  function stopTime(){
+
+    clearTimeout(a);
+  }
